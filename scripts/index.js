5c668993a92a237abd86497385e7d9a9060bc671
Ilya Petrikov <ilyapetrikov391@gmail.com>
2025-01-12 16:24:29 +0300

    main project

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,34 @@
 // @todo: Темплейт карточки
-
+const cardTemplate = document.querySelector('#card-template').content
 // @todo: DOM узлы
-
+const cards = document.querySelector('.places__list')
 // @todo: Функция создания карточки
 
+function createCard(data, removeCb) {
+	const cardElement = cardTemplate
+		.querySelector('.places__item')
+		.cloneNode(true)
+
+	cardElement.querySelector('.card__image').src = data.link
+	cardElement.querySelector('.card__image').alt = data.name
+	cardElement.querySelector('.card__title').textContent = data.name
+
+	const deleteButton = cardElement.querySelector('.card__delete-button')
+
+	deleteButton.addEventListener('click', function () {
+		removeCb(cardElement)
+	})
+
+	return cardElement
+}
 // @todo: Функция удаления карточки
+function deleteCard(item) {
+	item.remove()
+}
 
 // @todo: Вывести карточки на страницу
+function pageOn(cardsBlock) {
+	const cardNode = cardsBlock.map((item) => createCard(item, deleteCard))
+	cards.append(...cardNode)
+}
+pageOn(initialCards)
